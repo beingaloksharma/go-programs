aeb73ede2ccf6c22e9bcb0a13b970f8778b28bd2
Mr Engineer <alok.kumar3172@outlook.com>
2024-01-09 13:28:41 +0530

    map key example
    Signed-off-by: Mr Engineer <alok.kumar3172@outlook.com>

---
+++ eight.go
@@ -0,0 +1,16 @@
+// WAP
+// Check weather key exists in map
+
+package main
+
+import "fmt"
+
+func main() {
+	address := map[string]string{"alok": "New Delhi"}
+	_, ok := address["alok"]
+	if ok {
+		fmt.Println("Key is found ")
+	} else {
+		fmt.Println("Key is not found")
+	}
+}
--- seven.go
+++ seven.go
@@ -1,21 +1,21 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"sync"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// )
 
-func main() {
-	ch := make(chan string)
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
-	go func(ch <-chan string, wg *sync.WaitGroup) {
-		defer wg.Done()
-		fmt.Println(<-ch)
-	}(ch, wg)
-	go func(ch chan string, wg *sync.WaitGroup) {
-		defer wg.Done()
-		ch <- "Sending message to receiver channel"
-	}(ch, wg)
-	wg.Wait()
-}
+// func main() {
+// 	ch := make(chan string)
+// 	wg := &sync.WaitGroup{}
+// 	wg.Add(2)
+// 	go func(ch <-chan string, wg *sync.WaitGroup) {
+// 		defer wg.Done()
+// 		fmt.Println(<-ch)
+// 	}(ch, wg)
+// 	go func(ch chan string, wg *sync.WaitGroup) {
+// 		defer wg.Done()
+// 		ch <- "Sending message to receiver channel"
+// 	}(ch, wg)
+// 	wg.Wait()
+// }
